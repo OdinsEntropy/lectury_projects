23d60aaf776806c6bb9cf277abd1322993566940
Eric Pohl <advisor.pohl@gmail.com>
2025-02-10 21:44:25 +0100

    better naming scheme

--- pytorch/fundamentals/00_numpy_pytorch.py
+++ pytorch/fundamentals/00_numpy_pytorch.py
@@ -3,10 +3,11 @@
 # inspired by series https://www.youtube.com/watch?v=EMXfZB8FVUA&list=PLqnslRFeH2UrcDBWF5mfPGpqQDSta6VK4
 
 import torch
+import torch.nn as nn
 import numpy as np
 
 # Parameter definition
-mode = 'pytorch' # 'numpy' or 'pytorch'
+mode = 'pytorch' # 'numpy' or 'partial_pytorch' or 'pytorch'
 
 learning_rate = 0.1
 n_iters = 20
@@ -45,8 +46,8 @@ def numpy_model_implement(x,y,w):
 
     print(f'Prediction after training: f(5) = {forward(5):.3f}')
 
-
-def pytorch_model_implement(X,Y,w):
+# Partial Pytorch model implementation, calc loss, backward, update weight with negative gradient
+def pytorch_partial_model_implement(X,Y,w):
 
     def forward(x):
         return w*x
@@ -75,13 +76,47 @@ def pytorch_model_implement(X,Y,w):
 
     print(f'Prediction after training: f(5) = {forward(5):.3f}')
 
+# Full Pytorch model implementation, automatic differentiation, calc loss, backward, update weight with optimizer
+def pytorch_model_implement(X,Y):
+
+    class LinearRegressionOwn(nn.Module):
+            def __init__(self):
+                super(LinearRegressionOwn, self).__init__()
+                self.linear = nn.Linear(1,1)
+
+            def forward(self, x):
+                return self.linear(x)
+            
+    # Define model, loss function and optimizer
+    model = LinearRegressionOwn()
+    loss = nn.MSELoss()
+    optimizer = torch.optim.SGD(model.parameters(), lr=learning_rate)
+
+    for epoch in range(n_iters):
+        y_pred = model(X)
+        l = loss(Y, y_pred)
+
+        l.backward()
+
+        optimizer.step()
+        optimizer.zero_grad()
+
+        if epoch % 1 == 0:
+            [w,b] = model.parameters()
+            print(f'epoch {epoch+1}: w = {w[0][0].item():.3f}, loss = {l:.8f}')
+
+
+
 
 if __name__ == '__main__':
 
     if mode == 'numpy':
-        numpy_model_implement(X,Y,w)
+        numpy_model_implement(X,Y)
 
+    elif mode == 'partial_pytorch':
+        pytorch_partial_model_implement(torch.tensor(X),torch.tensor(Y),torch.tensor(w, requires_grad=True))
+    
     elif mode == 'pytorch':
-        pytorch_model_implement(torch.tensor(X),torch.tensor(Y),torch.tensor(w, requires_grad=True))
+        pytorch_model_implement(torch.tensor(X, dtype=torch.float32).view(-1,1),torch.tensor(Y, dtype=torch.float32).view(-1,1))
     else:
         print('Invalid mode')
