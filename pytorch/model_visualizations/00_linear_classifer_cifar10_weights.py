abb489e7f7d6f3513ac57b004f99d6234429a4d9
Eric Pohl <advisor.pohl@gmail.com>
2026-01-18 22:23:03 +0100

    init: Lecture about weight representation in cv using a linear classifier

---
+++ pytorch/model_visualizations/00_linear_classifer_cifar10_weights.py
@@ -0,0 +1,9 @@
+###
+# Created Date: Sun Jan 18 2026
+# Author Eric Pohl
+# 
+# Copyright (c) 2026 Eric Pohl
+# Abstract
+# This project implement an linear classifier with Wx + B
+# Train on CIFAR10 and visualize the change of W in regards to the classes
+###
